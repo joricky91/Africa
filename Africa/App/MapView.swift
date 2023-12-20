ffe80ec2c5eaafc3cbac0550f69997c916a14035
joricky91 <jonathanricky91@gmail.com>
2023-12-20 15:39:06 +0700

    feat: finishing custom annotation

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region: MapCameraPosition = {
+    @State private var regionPosition: MapCameraPosition = {
         var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
         var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
         var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
@@ -27,17 +27,67 @@ struct MapView: View {
     let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     
     var body: some View {
-        Map(position: $region) {
-            ForEach(locations) { location in
-                Annotation("", coordinate: location.location) {
-                    Image("logo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 32, height: 32, alignment: .center)
-                }
+//        Map(position: $regionPosition) {
+//            ForEach(locations) { location in
+//                Annotation(location.name, coordinate: location.location) {
+//                    MapAnnotationView(location: location)
+//                }
+//            }
+//        }
+        Map(coordinateRegion: $region2, annotationItems: locations) { item in
+        //            MapPin(coordinate: item.location, tint: .accentColor)
+        //            MapMarker(coordinate: item.location, tint: .accentColor)
+            MapAnnotation(coordinate: item.location) {
+                MapAnnotationView(location: item)
             }
         }
-        
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        
+                        Spacer()
+                        
+                        Text("\(region2.center.latitude )")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        
+                        Spacer()
+                        
+                        Text("\(region2.center.longitude )")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            , alignment: .top
+        )
 //        Map(coordinateRegion: $region2, annotationItems: locations) { item in
 ////            MapPin(coordinate: item.location, tint: .accentColor)
 ////            MapMarker(coordinate: item.location, tint: .accentColor)
